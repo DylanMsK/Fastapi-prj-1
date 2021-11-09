cbcf5e877a8d9d7723121097b50d71a1122eaad1
minsu kang <kms920612@gmail.com>
2021-11-09 23:35:42 +0900

    edit entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
- #!/bin/bash
+#!/bin/bash
 
- RUN_PORT=${PORT:-8000}
+RUN_PORT=${PORT:-8000}
 
- /opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}" app.main:app
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${RUN_PORT}" app.main:app
